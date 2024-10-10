58d7a6b92e6268abf58cd2b16acfc5b0c569acf4
WDragon277 <wdragon277@gmail.com>
2024-10-10 15:14:19 +0900

    1. 배치 스크립트 관리를 위한 브런치

--- models/sea_freight/test3.py
+++
@@ -1,6 +0,0 @@
-from common.utils.utils import searchAPI
-from common.utils.setting import EsSetting
-es = EsSetting()
-idx = es.sea_save_freight
-tmp = searchAPI(idx)
-tmp = tmp.sort_values(['data_cd','dptr_cnty','arvl_cnty','year_mon'])
